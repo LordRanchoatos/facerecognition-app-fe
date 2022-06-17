cb531df66a35ab93f08df8ab07cce575080bdb59
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-06-16 23:07:10 -0400

    connfigured react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -8,7 +9,7 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
-//import UserProfile from "./components/UserProfile/UserProfile";
+import UserProfile from "./components/UserProfile/UserProfile";
 
 const particlesOptions = {
   particles: {
@@ -16,16 +17,16 @@ const particlesOptions = {
       value: 200,
       density: {
         enable: true,
-        value_area: 800
-      }
-    }
-  }
+        value_area: 800,
+      },
+    },
+  },
 };
 
 const initialState = {
-  input: '',
-  imageUrl: '',
-  box: {}, 
+  input: "",
+  imageUrl: "",
+  box: {},
   route: "",
   isSignedIn: false,
   user: {
@@ -34,9 +35,9 @@ const initialState = {
     email: "",
     entries: 0,
     joined: "",
-    numberOfFaces: ""
-  }
-}
+    numberOfFaces: "",
+  },
+};
 
 // const showProfile = () => {
 //   this.setState({
@@ -51,124 +52,144 @@ class App extends Component {
     this.state = initialState;
   }
 
-
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
-  }
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
-  
   calculateFaceLocation = (data) => {
-    this.setState({numberOfFaces: data.outputs[0].data.regions.length})
+    this.setState({ numberOfFaces: data.outputs[0].data.regions.length });
     // console.log("datatolog: ", data.outputs[0].data.regions.length)
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log("image", width + ":" + height)
+    console.log("image", width + ":" + height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height),             
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   displayFaceBox = (box) => {
-    console.log(box)
-    this.setState({box: box})
-  }
+    console.log(box);
+    this.setState({ box: box });
+  };
 
-  onInputChange = event => {
-    this.setState({input: event.target.value});
-  }
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
 
   onRouteChange = (route) => {
-    if (route === "signout"){
-      this.setState(initialState)
-    } else if (route === "home"){
-      this.setState({isSignedIn: true})
+    if (route === "signout") {
+      this.setState(initialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState ({route: route});
-  }
-  
+    this.setState({ route: route });
+  };
 
   onButtonSubmit = () => {
-    console.log(this.state.user)
-    this.setState({imageUrl: this.state.input});  
+    console.log(this.state.user);
+    this.setState({ imageUrl: this.state.input });
     fetch("https://polar-wave-90283.herokuapp.com/imageUrl", {
       method: "post",
-      headers: {"content-Type": "application/json"},
+      headers: { "content-Type": "application/json" },
       body: JSON.stringify({
-        input: this.state.input
-      })
+        input: this.state.input,
+      }),
     })
-    .then(response => response.json())
-      .then(response => {
+      .then((response) => response.json())
+      .then((response) => {
         if (response) {
           fetch("https://polar-wave-90283.herokuapp.com/image", {
             method: "put",
-            headers: {"content-Type": "application/json"},
+            headers: { "content-Type": "application/json" },
             body: JSON.stringify({
               id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-          })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
-          })
-          .catch(console.log)
+            .catch(console.log);
         }
-      
-      this.displayFaceBox(this.calculateFaceLocation(response))
+
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
-
-
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
-    // let profile;
-    // if (this.route = "profile"){
-    //   profile = <UserProfile />
-    // } else {
-    //   profile = null
-    // }
     return (
-      <div className="App">
-        <Particles className="particles" params={particlesOptions} />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} showProfile={this.showProfile} name={this.state.user.name}/>
-        {route === "home"? 
-          <div>
-            <Logo />
-            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
-              <ImageLinkForm
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-                <FaceRecognition box={box} imageUrl={imageUrl} noOfFaces={this.state.numberOfFaces}/>
-          </div>:
-            (route === "signin"?
-              <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/> :
-              <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-            )
-          
-        }
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <div className="App">
+                <Particles className="particles" params={particlesOptions} />
+                <Navigation
+                  isSignedIn={isSignedIn}
+                  onRouteChange={this.onRouteChange}
+                  showProfile={this.showProfile}
+                  name={this.state.user.name}
+                />
+                {route === "home" ? (
+                  <div>
+                    <Logo />
+                    <Rank
+                      name={this.state.user.name}
+                      entries={this.state.user.entries}
+                    />
+                    <ImageLinkForm
+                      onInputChange={this.onInputChange}
+                      onButtonSubmit={this.onButtonSubmit}
+                    />
+                    <FaceRecognition
+                      box={box}
+                      imageUrl={imageUrl}
+                      noOfFaces={this.state.numberOfFaces}
+                    />
+                  </div>
+                ) : route === "signin" ? (
+                  <Signin
+                    loadUser={this.loadUser}
+                    onRouteChange={this.onRouteChange}
+                  />
+                ) : (
+                  <Register
+                    loadUser={this.loadUser}
+                    onRouteChange={this.onRouteChange}
+                  />
+                )}
+              </div>
+            </Route>
+
+            <Route path="/profile">
+              <UserProfile isSignedIn={isSignedIn}
+                  onRouteChange={this.onRouteChange}
+                  showProfile={this.showProfile}
+                  name={this.state.user.name}/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
 export default App;
-
-
-
-
 
 // app.models
 //       .predict(
@@ -187,12 +208,11 @@ export default App;
 //           console.log(response);
 //         },
 //         function(err) {
-//           // there was an error 
+//           // there was an error
 //         }
 //       );
-
 
 // topRow: (clarifaiFace.top_row * height),
 //       leftCol: (clarifaiFace.left_col * width),
 //       rightCol: (width - clarifaiFace.right_col * width),
-//       bottomRow: (height - height * clarifaiFace.bottom_row)     
+//       bottomRow: (height - height * clarifaiFace.bottom_row)
--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
-function UserProfile() {
+function UserProfile({ onRouteChange, isSignedIn, name }) {
   return (
-    <div>UserProfile</div>
+    <div>
+    <Navigation onRouteChange={onRouteChange}/>
+        <div>THIS THE USER profile PAGE</div>
+    </div>
   )
 }
 
