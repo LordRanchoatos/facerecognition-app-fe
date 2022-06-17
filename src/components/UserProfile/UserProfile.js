24b3b533d3cc8e423052d0fe7d21bd0455b91414
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-06-17 01:22:37 -0400

    format date display:profile page

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import dayjs from 'dayjs'
+var relativeTime = require('dayjs/plugin/relativeTime')
+
 
 function UserProfile({ onRouteChange, isSignedIn, name, id, email, entries, joined }) {
-    console.log('IS SIGNED IN: ', isSignedIn)
+    const date = dayjs(joined).format('DD/MM/YYYY')
+    dayjs.extend(relativeTime)
+    const fNow = dayjs(joined).fromNow()
+
+
   return (
     <div>
     <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} name={name}/>
@@ -17,7 +18,7 @@ function UserProfile({ onRouteChange, isSignedIn, name, id, email, entries, join
             <h3>Hey! {name}</h3>
             <p>User Id email: {id}</p>
             <p>registered email: {email}</p>
-            <p>joined: {joined}</p>
+            <p>joined: {date} ({fNow})</p>
             <p>Numbers of entries so far email: {entries}</p>
         </div> : <div>
             <h3>Hey friend you have to be login first</h3>
